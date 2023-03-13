4158cc7bd9311b5ea9c3c654d31f7b9efeb21b46
ijeremic <ijeremic89@gmail.com>
2023-03-13 11:56:31 +0100

    Change store on submit

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,13 +1,35 @@
-import { Component, Fragment } from "react";
+import React, { Fragment } from "react";
+import { getFunName } from "../helpers";
 
-class StorePicker extends Component {
+class StorePicker extends React.Component {
+
+  myInput = React.createRef();
+
+  goToStore = (event) => {
+    // 1. Stop the form from submiting
+    event.preventDefault();
+
+    // 2. Get the text from the input
+    const storeName = this.myInput.current.value;
+
+    // 3. Change the page to /store/whatever-they-entered
+    this.props.history.push(`/store/${storeName}`)
+  }
+
   render() {
     return (
         <Fragment>
           <h2>Hello, please Enter s store</h2>
-          <form className="store-selector">
-            <input type="text" required placeholder="Store Name"/>
-            <button type="submit">Visit store -></button>
+          <form className="store-selector"
+                onSubmit={this.goToStore}>
+            <input type="text"
+                   ref={this.myInput}
+                   required
+                   placeholder="Store Name"
+                   defaultValue={getFunName()}/>
+            <button type="submit"
+                    onClick={this.handleClick}>Visit store ->
+            </button>
           </form>
         </Fragment>
     );
